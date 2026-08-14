7f7ca438bde6850c4032b51d3ca0d12e00433249
shimwayvan <shimwayvan@gmail.com>
2026-08-14 12:05:18 +0300

    new component for new messages

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,10 @@ import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
 import './App.css'
 
-function App({isloggedin}) {
+function App({count}) {
   return (
     <div className="App">
-      {isloggedin ? <h1>Welcome to the App</h1> : <h1>Please log in to continue</h1>}
+      {count > 0 && <p> You have {count} new m</p>}
     </div>    
   )
 }
@@ -15,7 +15,26 @@ function App({isloggedin}) {
 export default App
 
 
-//function App() {
+function App({count}) {
+  return (
+    <div className="App">
+      {count > 0 && <p> You have {count} new m</p>}
+    </div>    
+  )
+}
+
+
+
+// function App({isloggedin}) { This is the App component that takes a prop called isloggedin. It conditionally renders a welcome message if the user is logged in, or a prompt to log in if they are not. The component is exported as the default export of the module.
+//   return (
+//     <div className="App">
+//       {isloggedin ? <h1>Welcome to the App</h1> : <h1>Please log in to continue</h1>}
+//     </div>    
+//   )
+// }
+
+
+//function App() {  This is an alternative version of the App component that uses the useState hook to manage a count state variable. It also defines an array of player names and renders them in an unordered list. The component is commented out, so it is not currently being used in the application.
   // const [count, setCount] = useState(0)
   // const p = ['Messi', 'Ronaldo', 'Neymar', 'Mbappe', 'Lewandowski', 'Kane', 'Salah', 'De Bruyne', 'Modric', 'Benzema']
 
@@ -30,3 +49,4 @@ export default App
   //   </>
   // )
 //}
+
